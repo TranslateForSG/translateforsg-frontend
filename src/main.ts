c33cdade05841020212b33b8c633fbae5c994642
Aniruddha Adhikary <aniruddha@saleswhale.com>
2020-04-29 13:42:41 +0800

    fix sentry dsn delcaration

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ import * as Sentry from '@sentry/browser';
 import { Vue as VueIntegration } from '@sentry/integrations';
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn: process.env.VUE_APP_SENTRY_DSN,
   integrations: [new VueIntegration({Vue, attachProps: true})],
 });
 
