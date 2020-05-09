c303048a06eca89abd1907946f61cd1d3061700d
Aniruddha Adhikary <aniruddha@adhikary.net>
2020-05-09 13:52:58 +0800

    update service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,18 @@
-const {registerRoute} = workbox.routing;
-const {NetworkFirst, CacheFirst} = workbox.strategies;
+const { registerRoute } = workbox.routing;
+const { NetworkFirst, CacheFirst } = workbox.strategies;
 
-workbox.core.setCacheNameDetails({prefix: "TranslateForSG"});
+workbox.core.setCacheNameDetails({ prefix: "TranslateForSG" });
 
 workbox.core.skipWaiting();
+workbox.core.clientsClaim();
 
 registerRoute(
-  ({url}) => url.pathname.startsWith('/api/'),
+  ({ url }) => url.pathname.startsWith('/api/'),
   new NetworkFirst()
 );
 
 registerRoute(
-  ({url}) => url.hostname === 'storage.googleapis.com' && url.pathname.startsWith('/translateforsg-data/'),
+  ({ url }) => url.hostname === 'storage.googleapis.com' && url.pathname.startsWith('/translateforsg-data/'),
   new CacheFirst()
 );
 
